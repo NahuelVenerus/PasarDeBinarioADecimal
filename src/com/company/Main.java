a83f965b6ff00a0541a1da41536cd53c64f54aa6
Nahuel Venerus <nahuelvenerus@gmail.com>
2020-07-13 16:54:02 -0300

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,10 @@ public class Main {
     Calculo: 1010010 = 64 + 16 + 2 = 82
     Ejemplo: 1010010 = 82
      */
-    public static String invertirNumeroBinario(String numeroBinario){
-        String binarioInvertido = "";
-        for (int i = numeroBinario.length()-1; i >= 0; i--) {
-            binarioInvertido += numeroBinario.charAt(i);
-        }
-        return binarioInvertido;
-    }
-
-    public static int transformarBinarioADecimal(String binarioInvertido){
+    public static int transformarBinarioADecimal(String numeroBinario){
         int decimal = 0;
-        for (int i = 0; i < binarioInvertido.length(); i++) {
-            if (binarioInvertido.charAt(i) == '1'){
+        for (int i = numeroBinario.length(); i < 0; i++) {
+            if (numeroBinario.charAt(i) == '1'){
                 decimal += Math.pow(2,i);
             }
         }
@@ -27,14 +19,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         int numeroDecimal;
         String numeroBinario = "";
         String binarioInvertido = "";
         Scanner reader = new Scanner(System.in);
         numeroBinario = reader.nextLine();
-
-        binarioInvertido = invertirNumeroBinario(numeroBinario);
         numeroDecimal = transformarBinarioADecimal(binarioInvertido);
 
         System.out.println(numeroDecimal);
